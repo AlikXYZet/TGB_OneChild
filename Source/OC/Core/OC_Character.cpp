a948684e4d477b9dab6dc176dd560b90d8d94fdf
Александр Осипов <alikxyzet@gmail.com>
2024-10-20 04:39:18 +0300

    0.1.1 Add Starting Elements: * Character; * Widgets; * Localization.

--- Source/OC/Core/OC_Character.cpp
+++ Source/OC/Core/OC_Character.cpp
@@ -1,34 +1,130 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
+// Base:
 #include "OC_Character.h"
 
-// Sets default values
+// UE:
+#include "Components/CapsuleComponent.h"
+#include "Camera/CameraComponent.h"
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Constructors   --- */
+
 AOC_Character::AOC_Character()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	// Установка вызова функции Tick() в каждом кадре.
+	PrimaryActorTick.bCanEverTick = false; // Предварительно
 
+
+	/* ---   Components   --- */
+
+	// Create a CameraComponent	
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(-39.56f, 1.75f, 64.f)); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+	GetMesh()->SetupAttachment(FirstPersonCameraComponent);
+	//-------------------------------------------
+
+
+	//Cleaning();
 }
+//--------------------------------------------------------------------------------------
 
-// Called when the game starts or when spawned
+
+
+/* ---   Base   --- */
+
 void AOC_Character::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
-// Called every frame
-void AOC_Character::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
+//void AOC_Character::Cleaning()
+//{
+//	// Резерв
+//}
+//--------------------------------------------------------------------------------------
 
-}
 
-// Called to bind functionality to input
+
+/* ---   Input Action   --- */
+
 void AOC_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	// Прыжок:
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AOC_Character::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AOC_Character::StopJumping);
+
+	// Присед:
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &AOC_Character::StartCrouch);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &AOC_Character::StopCrouch);
+
+	// Перемещение:
+	PlayerInputComponent->BindAxis("MoveForward", this, &AOC_Character::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AOC_Character::MoveRight);
+
+	// Поворот мышью:
+	PlayerInputComponent->BindAxis("TurnRate", this, &AOC_Character::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AOC_Character::LookUpAtRate);
 }
 
+void AOC_Character::Jump()
+{
+	Super::Jump();
+}
+
+void AOC_Character::StopJumping()
+{
+	Super::StopJumping();
+}
+
+void AOC_Character::MoveForward(float Value)
+{
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
+}
+
+void AOC_Character::MoveRight(float Value)
+{
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetActorRightVector(), Value);
+	}
+}
+
+void AOC_Character::TurnAtRate(float Rate)
+{
+	// Вычисление дельты для текущего кадра на основе информации о скорости
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AOC_Character::LookUpAtRate(float Rate)
+{
+	// Вычисление дельты для текущего кадра на основе информации о скорости
+	AddControllerPitchInput(-Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AOC_Character::StartCrouch()
+{
+	// Предваарительно: Из стандартного кода UE
+	Super::Crouch();
+	// Remark: Желательно заменить на плавный присед персонажа
+}
+
+void AOC_Character::StopCrouch()
+{
+	// Предваарительно: Из стандартного кода UE
+	Super::UnCrouch();
+	// Remark: Желательно заменить на плавный присед персонажа
+}
+//--------------------------------------------------------------------------------------
--- Source/OC/Core/OC_Character.h
+++ Source/OC/Core/OC_Character.h
@@ -1,10 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
+// Base:
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
+// Generated:
 #include "OC_Character.generated.h"
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Pre-declaration of classes   --- */
+
+// UE:
+class UCameraComponent;
+//--------------------------------------------------------------------------------------
+
+
 
 UCLASS()
 class OC_API AOC_Character : public ACharacter
@@ -12,18 +26,87 @@ class OC_API AOC_Character : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+
+	/* ---   Constructors   --- */
+
+	// Устанавливает значения по умолчанию для свойств этого персонажа
 	AOC_Character();
+	//-------------------------------------------
+
+
+
+	/* ---   Components   --- */
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera,
+		meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FirstPersonCameraComponent;
+	//-------------------------------------------
+
+
 
 protected:
-	// Called when the game starts or when spawned
+
+	/* ---   Base   --- */
+
+	// Вызывается при запуске игры или при запуске spawn
 	virtual void BeginPlay() override;
+	//-------------------------------------------
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
+
+	/* ---   Input Action   --- */
+
+	/** Вызывается для привязки функциональности к входным данным */
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	/** Запуск Прыжка */
+	virtual void Jump() override;
+
+	/** Прекращение Прыжка */
+	virtual void StopJumping() override;
+
+	/** Перемещение вперёд/назад */
+	void MoveForward(float Val);
+
+	/** Перемещение влево/вправо */
+	void MoveRight(float Val);
+
+	/**	Поворот с заданной скоростью */
+	void TurnAtRate(float Rate);
+
+	/**	Обзор вверх/вниз с заданной скоростью */
+	void LookUpAtRate(float Rate);
+
+	/** Начало Приседа */
+	void StartCrouch();
+
+	/** Прекращение Приседа */
+	void StopCrouch();
+	//-------------------------------------------
+
+
+
+public:
+
+	/* ---   Input Action   --- */
+
+	// Базовая скорость поворота, град/сек
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseTurnRate = 45.f;
+
+	// Базовая скорость обзора вверх/вниз в градусах в секунду
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseLookUpRate = 45.f;
+	//-------------------------------------------
+
+
+
+private:
+
+	/* ---   Base   --- */
+
+	/**	Удаление пустых элементов текущего Актора */
+	//void Cleaning();
+	//-------------------------------------------
 };
