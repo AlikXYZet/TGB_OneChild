64b30dd576711afaaf5ceeb34b95d6407d419791
Александр Осипов <alikxyzet@gmail.com>
2024-10-22 00:03:54 +0300

    0.1.2 Add Starting Elements: * NoisyCoatings.

---
+++ Source/OC/Interactive/Noisy/NoisyCoatings.cpp
@@ -0,0 +1,141 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+// Base:
+#include "NoisyCoatings.h"
+
+// UE:
+#include "Kismet/GameplayStatics.h"
+#include "Components/BoxComponent.h"
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Constructors   --- */
+
+ANoisyCoatings::ANoisyCoatings()
+{
+	// Установка вызова функции Tick() в каждом кадре.
+	PrimaryActorTick.bCanEverTick = false; // Предварительно
+
+
+
+	/* ---   Components   --- */
+
+	// Корневой компонент
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	// Меш-компонент данного актора
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+	Mesh->SetCollisionProfileName(TEXT("OverlapAll"));
+
+	// Компонент коллизии при отсутствии Меша
+	BoxTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Trigger"));
+	BoxTrigger->SetupAttachment(RootComponent);
+	BoxTrigger->SetRelativeLocation(FVector(0.f, 0.f, 3.f));
+	BoxTrigger->SetRelativeScale3D(FVector(1.f, 1.f, 0.1f));
+	//-------------------------------------------
+}
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Base   --- */
+
+void ANoisyCoatings::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Checking();
+}
+
+void ANoisyCoatings::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	Preview();
+}
+
+void ANoisyCoatings::Preview()
+{
+	FTypeCoating* lCurrentSettingType = Types.Find(CurrentType);
+
+	if (lCurrentSettingType && lCurrentSettingType->CollisionMesh)
+	{
+		UStaticMesh* lSelectedMesh = lCurrentSettingType->CollisionMesh;
+
+		if (lSelectedMesh != Mesh->GetStaticMesh())
+		{
+			Mesh->SetStaticMesh(lCurrentSettingType->CollisionMesh);
+		}
+	}
+}
+
+void ANoisyCoatings::Checking()
+{
+	FTypeCoating* lCurrentSettingType = Types.Find(CurrentType);
+
+	if (lCurrentSettingType)
+	{
+		if (lCurrentSettingType->CollisionMesh)
+		{
+			BoxTrigger->DestroyComponent();
+		}
+		else
+		{
+			Mesh->SetStaticMesh(nullptr);
+			UE_LOG(LogTemp, Log, TEXT("'%s' has no Mesh. Custom collision will be enabled."), *GetNameSafe(this));
+		}
+
+		if (!lCurrentSettingType->WalkingSound)
+		{
+			UE_LOG(LogTemp, Error, TEXT("'%s' has no Sound"), *GetNameSafe(this));
+		}
+	}
+}
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Collision   --- */
+
+void ANoisyCoatings::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	PlaySound();
+}
+
+void ANoisyCoatings::PlaySound()
+{
+	if (Types.Find(CurrentType)->WalkingSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(
+			GetWorld(),
+			Types.Find(CurrentType)->WalkingSound,
+			GetActorLocation());
+	}
+}
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Type   --- */
+
+TArray<FString> ANoisyCoatings::GetAllType() const
+{
+	TArray<FString> rAllType;
+
+	for (auto Data : Types)
+	{
+		rAllType.Add(Data.Key);
+	}
+
+	if (!rAllType.IsValidIndex(0))
+	{
+		rAllType.Add("NONE");
+	}
+
+	return rAllType;
+}
+//--------------------------------------------------------------------------------------
---
+++ Source/OC/Interactive/Noisy/NoisyCoatings.h
@@ -0,0 +1,141 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+// Base:
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+// Generated:
+#include "NoisyCoatings.generated.h"
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Pre-declaration of classes   --- */
+
+// UE:
+class UBoxComponent;
+//--------------------------------------------------------------------------------------
+
+
+
+/* ---   Custom Structs   --- */
+
+//
+USTRUCT(BlueprintType)
+struct FTypeCoating
+{
+	GENERATED_BODY()
+
+	// Меш структуры взаимодействия
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMesh* CollisionMesh = nullptr;
+
+	// Звук ходьбы по данному "покрытию"
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USoundBase* WalkingSound = nullptr;
+};
+//--------------------------------------------------------------------------------------
+
+
+
+UCLASS()
+class OC_API ANoisyCoatings : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	/* ---   Constructors   --- */
+
+	// Sets default values for this actor's properties
+	ANoisyCoatings();
+	//-------------------------------------------
+
+
+
+	/* ---   Components   --- */
+
+	// Меш-компонент данного Актора
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components,
+		meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* Mesh;
+
+	// Компонент коллизии при отсутствии Меша
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Components,
+		meta = (AllowPrivateAccess = "true"))
+	UBoxComponent* BoxTrigger = nullptr;
+	//-------------------------------------------
+
+
+
+protected:
+
+	/* ---   Base   --- */
+
+	// Вызывается при запуске игры или при запуске spawn
+	virtual void BeginPlay() override;
+	//-------------------------------------------
+
+
+
+	/* ---   Type   --- */
+
+	// Выбор текущего типа
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Settings,
+		meta = (GetOptions = "GetAllType"))
+	FString CurrentType;
+
+	// Настраиваемый тип по имени
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Settings)
+	TMap<FString, FTypeCoating> Types;
+
+	//
+
+	/** Получить все типы */
+	UFUNCTION()
+	TArray<FString> GetAllType() const;
+	//-------------------------------------------
+
+
+
+public:
+
+	/* ---   Base   --- */
+
+	/** Вызывается при размещении экземпляра данного класса (в редакторе) или его запуске.
+	@param	Transform	-- Трансформация данного Актора.
+	*/
+	virtual void OnConstruction(const FTransform& Transform) override;
+	//-------------------------------------------
+
+
+
+	/* ---   Collision   --- */
+
+	/**	Событие, когда этот Актор перекрывается с другим */
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+	//-------------------------------------------
+
+
+
+private:
+
+	/* ---   Base   --- */
+
+	/** Предварительное визуальное отображение */
+	void Preview();
+
+	/** Проверка и чистка Актора */
+	void Checking();
+	//-------------------------------------------
+
+
+
+	/* ---   Collision   --- */
+
+	/** Воспроизведение звука, соответствующий выбранному типу */
+	void PlaySound();
+	//-------------------------------------------
+};
